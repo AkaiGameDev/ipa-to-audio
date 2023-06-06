dcb2c8e255322b60f3f202c4cca663b3d00271fa
Aaron Cai <aaroncai2012@gmail.com>
2023-06-06 13:45:35 -0400

    removed hello function

--- handler.py
+++ handler.py
@@ -11,14 +11,3 @@ def lambda_handler(event, context):
         TextType='ssml',
         VoiceId='Joey')
     return response
-
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
-    }
-
-    response = {"statusCode": 200, "body": json.dumps(body)}
-
-    return response
-
